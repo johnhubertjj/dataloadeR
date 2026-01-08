d80d892bb2fb07669624b9c08c040ee9792abbfa
John Hubert <jjh@vrmh.co.uk>
2026-01-08 22:46:57 +0000

    test: added ReadInDataset tests

--- tests/testthat/test-ReadInDataset.R
+++ tests/testthat/test-ReadInDataset.R
@@ -12,3 +12,18 @@ test_that("feather read-in works", {
   expect_equal(2 * 2, 4)
 })
 
+test_that("RData read-in works", {
+  table1 <- base::mget( load('testdata/cars.RData'))
+  table2 <- read_in_dataset(file = 'testdata/cars.RData', method = 'RData')
+  expect_equal(table1,table2)
+  expect_equal(2 * 2, 4)
+})
+
+test_that("RData read-in works", {
+  table1 <- base::mget( load('testdata/cars.rds'))
+  table2 <- read_in_dataset(file = 'testdata/cars.rds', method = 'RData')
+  expect_equal(table1,table2)
+  expect_equal(2 * 2, 4)
+})
+
+
